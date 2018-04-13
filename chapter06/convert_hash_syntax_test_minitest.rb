93b85f40889414b7bbbd0ab545b7dbe483676a70
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2018-04-13 12:23:28 +0900

    convert_hash_syntax_program

---
+++ chapter06/convert_hash_syntax_test_minitest.rb
@@ -0,0 +1,22 @@
+require 'minitest/autorun'
+require './chapter06/convert_hash_syntax'
+
+class ConvertHashSyntaxTest < Minitest::Test
+  def test_convert_hash_syntax
+    old_syntax = <<~TEXT
+    {
+      :name => 'Daido',
+      :age=>28,
+      :gender  =>  :male
+    }
+    TEXT
+    expected = <<~TEXT
+    {
+      name: 'Daido',
+      age: 28,
+      gender: :male
+    }
+    TEXT
+    assert_equal expected, convert_hash_syntax(old_syntax)
+  end
+end
