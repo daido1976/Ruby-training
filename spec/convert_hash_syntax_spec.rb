194ce525e0aaa59dfee3d20ebde3fb3aaa89a77e
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2018-04-13 15:50:36 +0900

    RSpecファイル内のローカル変数をletで置き換え

--- spec/convert_hash_syntax_spec.rb
+++ spec/convert_hash_syntax_spec.rb
@@ -1,21 +1,27 @@
 require './chapter06/convert_hash_syntax'
 
 RSpec.describe '#convert_hash_syntax' do
-  it 'convert old_syntax to expected' do
-    old_syntax = <<~TEXT
+  let(:old_syntax) {
+    <<~TEXT
     {
       :name => 'Daido',
       :age=>28,
       :gender  =>  :male
     }
     TEXT
-    expected = <<~TEXT
+  }
+
+  let(:expected) {
+    <<~TEXT
     {
       name: 'Daido',
       age: 28,
       gender: :male
     }
     TEXT
+  }
+
+  it 'convert old_syntax to expected' do
     expect(expected).to eq convert_hash_syntax(old_syntax)
   end
 end
