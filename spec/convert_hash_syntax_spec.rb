cd7571e492cdbd35d1b46394a7c0b44c37205e12
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2018-04-17 10:06:50 +0900

    RSpecテストのit以降の説明文変更に合わせてletの命名を変更

--- spec/convert_hash_syntax_spec.rb
+++ spec/convert_hash_syntax_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe '#convert_hash_syntax' do
     TEXT
   }
 
-  let(:converted_syntax) {
+  let(:new_syntax) {
     <<~TEXT
     {
       name: 'Daido',
@@ -22,6 +22,6 @@ RSpec.describe '#convert_hash_syntax' do
   }
 
   it 'ハッシュ記法を古い記法から新しい記法に変換する' do
-    expect(convert_hash_syntax(old_syntax)).to eq converted_syntax
+    expect(convert_hash_syntax(old_syntax)).to eq new_syntax
   end
 end
