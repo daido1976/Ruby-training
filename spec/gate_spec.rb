aa4384d09197b9a955ffebf2f6abf751c7e7463c
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2018-04-17 17:39:36 +0900

    contextのまとまりを切符の購入代と乗降駅に分け、それぞれの変数ticketをletに置き換え

--- spec/gate_spec.rb
+++ spec/gate_spec.rb
@@ -6,35 +6,39 @@ RSpec.describe Gate do
   let(:juso) { Gate.new(:juso) }
   let(:mikuni) { Gate.new(:mikuni) }
 
-  context '150円の切符を購入して梅田で乗車し、十三で降車した場合' do
-    it '出場できる(OK)' do
-      ticket = Ticket.new(150)
-      umeda.enter(ticket)
-      expect(juso.exit(ticket)).to be_truthy
+  context '150円の切符を購入した場合' do
+    let(:ticket) { Ticket.new(150) }
+
+    context '梅田で乗車し、十三で降車した場合' do
+      it '出場できる(OK)' do
+        umeda.enter(ticket)
+        expect(juso.exit(ticket)).to be_truthy
+      end
+    end
+
+    context '梅田で乗車し、三国で降車した場合' do
+      it '出場できない(NG)' do
+        umeda.enter(ticket)
+        expect(mikuni.exit(ticket)).to be_falsey
+      end
+    end
+
+    context '十三で乗車し、三国で降車した場合' do
+      it '出場できる(OK)' do
+        juso.enter(ticket)
+        expect(mikuni.exit(ticket)).to be_truthy
+      end
     end
   end
 
-  context '150円の切符を購入して梅田で乗車し、三国で降車した場合' do
-    it '出場できない(NG)' do
-      ticket = Ticket.new(150)
-      umeda.enter(ticket)
-      expect(mikuni.exit(ticket)).to be_falsey
-    end
-  end
+  context '190円の切符を購入した場合' do
+    let(:ticket) { Ticket.new(190) }
 
-  context '190円の切符を購入して梅田で乗車し、三国で降車した場合' do
-    it '出場できる(OK)' do
-      ticket = Ticket.new(190)
-      umeda.enter(ticket)
-      expect(mikuni.exit(ticket)).to be_truthy
-    end
-  end
-
-  context '150円の切符を購入して十三で乗車し、三国で降車した場合' do
-    it '出場できる(OK)' do
-      ticket = Ticket.new(150)
-      juso.enter(ticket)
-      expect(mikuni.exit(ticket)).to be_truthy
+    context '梅田で乗車し、三国で降車した場合' do
+      it '出場できる(OK)' do
+        umeda.enter(ticket)
+        expect(mikuni.exit(ticket)).to be_truthy
+      end
     end
   end
 end
