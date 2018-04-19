499297687f349d562be8c4e466d76890b697b4af
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2018-04-19 18:25:49 +0900

    RSpecテスト文の意味が通りやすくなるように、letの命名を変更

--- spec/deep_freezable_spec.rb
+++ spec/deep_freezable_spec.rb
@@ -2,19 +2,19 @@ require './chapter08/deep_freezable'
 
 RSpec.describe DeepFreezable do
   include DeepFreezable
-  
-  let(:countries) { ['Japan', 'US', 'India'] }
-  let(:currencies) { { 'Japan' => 'yen', 'US' => 'dollar', 'India' => 'rupee' } }
+
+  let(:array) { ['Japan', 'US', 'India'] }
+  let(:hash) { { 'Japan' => 'yen', 'US' => 'dollar', 'India' => 'rupee' } }
 
   describe '#deep_freeze' do
-    it '配列自身(countries)と配列の各要素(country)がfreezeされる' do
-      expect(deep_freeze(countries).frozen?).to be_truthy
-      expect(deep_freeze(countries).all? { |country| country.frozen? }).to be_truthy
+    it '配列自身(array)と配列の各要素(element)がfreezeされる' do
+      expect(deep_freeze(array).frozen?).to be_truthy
+      expect(deep_freeze(array).all? { |element| element.frozen? }).to be_truthy
     end
 
-    it 'ハッシュ自身(currencies)とハッシュの各要素(key,value)がfreezeされる' do
-      expect(deep_freeze(currencies).frozen?).to be_truthy
-      expect(deep_freeze(currencies).all? { |key, value| key.frozen? && value.frozen? }).to be_truthy
+    it 'ハッシュ自身(hash)とハッシュの各要素(key,value)がfreezeされる' do
+      expect(deep_freeze(hash).frozen?).to be_truthy
+      expect(deep_freeze(hash).all? { |key, value| key.frozen? && value.frozen? }).to be_truthy
     end
   end
 end
