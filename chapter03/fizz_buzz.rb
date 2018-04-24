d6e8831694742e674f6016594ef1548d0cd61885
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2018-04-24 17:21:58 +0900

    num % x == 0は(num % x).zero?に書き換える by RuboCop

--- chapter03/fizz_buzz.rb
+++ chapter03/fizz_buzz.rb
@@ -1,9 +1,9 @@
 def fizz_buzz(num)
-  if num % 15 == 0
+  if (num % 15).zero?
     'Fizz Buzz'
-  elsif num % 3 == 0
+  elsif (num % 3).zero?
     'Fizz'
-  elsif num % 5 == 0
+  elsif (num % 5).zero?
     'Buzz'
   else
     num.to_s
