ab2239a32ad68e8e4cc8c7c70faa84f57c796a1a
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2018-04-25 14:48:10 +0900

    RSpecテスト文を場合別にcontextで分け、stubをbeforeで切り出し

--- spec/regexp_checker_spec.rb
+++ spec/regexp_checker_spec.rb
@@ -4,16 +4,20 @@ RSpec.describe RegexpChecker do
   let(:regexp_checker) { RegexpChecker.new }
 
   describe '#check_regexp' do
-    it '正規表現にマッチしていれば、マッチした文字列をカンマ区切りで返す' do
-      regexp_checker.stub(:input_text).and_return('123abc456def789')
-      regexp_checker.stub(:input_regexp).and_return(/\d\d\d/)
-      expect { regexp_checker.check_regexp }.to output("Matched: 123, 456, 789\n").to_stdout
+    before { regexp_checker.stub(:input_text).and_return('123abc456def789') }
+
+    context '入力した文字列が正規表現にマッチしている場合' do
+      before { regexp_checker.stub(:input_regexp).and_return(/\d\d\d/) }
+      it 'マッチした文字列をカンマ区切りで返す' do
+        expect { regexp_checker.check_regexp }.to output("Matched: 123, 456, 789\n").to_stdout
+      end
     end
 
-    it '正規表現にマッチしていなければ、"Nothing matched."という文字列を返す' do
-      regexp_checker.stub(:input_text).and_return('123abc456def789')
-      regexp_checker.stub(:input_regexp).and_return(/z+/)
-      expect { regexp_checker.check_regexp }.to output("Nothing matched.\n").to_stdout
+    context '入力した文字列が正規表現にマッチしていない場合' do
+      before { regexp_checker.stub(:input_regexp).and_return(/z+/) }
+      it '"Nothing matched."という文字列を返す' do
+        expect { regexp_checker.check_regexp }.to output("Nothing matched.\n").to_stdout
+      end
     end
   end
 end
